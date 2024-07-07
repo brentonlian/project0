b2a356e651607fe32cc3813884c9b9680cc25061
Brenton Lian <brenton.lian@gmail.com>
2024-07-07 14:28:33 -0400

    Attempted to add menu

---
+++ app/Sources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sources = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Sources</h1>
+      <p>Here you can list the sources of your data.</p>
+    </div>
+  );
+};
+
+export default Sources;
