753cdc7e1ac76c0d2358a2da6f00fe601ebf8ec8
Brenton Lian <brenton.lian@gmail.com>
2024-07-13 11:34:22 -0400

    tried to merge styling. Functionality is present

---
+++ app/hooks/useStorageCalculator.js
@@ -0,0 +1,77 @@
+// app/hooks/useStorageCalculator.js
+'use client';
+
+import { useState, useEffect } from 'react';
+import { loadCSV } from '../../utils/loadCSV';
+
+const useStorageCalculator = () => {
+  const [data, setData] = useState([]);
+  const [result, setResult] = useState(null);
+  const [decadeInfo, setDecadeInfo] = useState(null);
+  const [decade, setDecade] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const csvData = await loadCSV('/data.csv');
+      setData(csvData);
+    };
+    fetchData();
+  }, []);
+
+  const handleCalculate = async ({ amount, unit, year, storageType }) => {
+    const yearData = data.find((row) => row.Year === year);
+    if (yearData) {
+      const costPerTerabyte = parseFloat(yearData[storageType]);
+      if (isNaN(costPerTerabyte)) {
+        setResult("Cost data not available for selected storage type and year.");
+        return;
+      }
+      const amountInTerabytes = convertToTerabytes(parseFloat(amount), unit);
+      const totalCost = costPerTerabyte * amountInTerabytes;
+      const resultMessage = `The estimated cost of ${amount} ${unit} of ${storageType} storage in ${year} was $${totalCost.toFixed(2)}.`;
+      setResult(resultMessage);
+
+      const calculatedDecade = getDecade(year);
+      setDecade(calculatedDecade);
+      const decadeData = await fetchDecadeInfo(calculatedDecade);
+      if (decadeData) {
+        setDecadeInfo(decadeData);
+      } else {
+        setDecadeInfo(null);
+      }
+    } else {
+      setResult("Year not found in data.");
+    }
+  };
+
+  const convertToTerabytes = (amount, unit) => {
+    switch (unit) {
+      case 'GB':
+        return amount / 1024;
+      case 'MB':
+        return amount / (1024 * 1024);
+      default:
+        return amount;
+    }
+  };
+
+  const getDecade = (year) => {
+    const decade = Math.floor(year / 10) * 10;
+    return `${decade}s`;
+  };
+
+  const fetchDecadeInfo = async (decade) => {
+    const response = await fetch('/decadesInfo.json');
+    const data = await response.json();
+    return data[decade];
+  };
+
+  return {
+    result,
+    decadeInfo,
+    decade,
+    handleCalculate,
+  };
+};
+
+export default useStorageCalculator;
