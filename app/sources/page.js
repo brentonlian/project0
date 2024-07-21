2f7b0e5863d667f7b429d0eabbcce71fb3955598
Brenton Lian <brenton.lian@gmail.com>
2024-07-21 16:07:05 -0400

    full func just need to add sources and red errors on main page

--- app/sources/page.js
+++ app/sources/page.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from './components/Header';
+import Header from '../components/Header';
 
 const Sources = () => {
   return (
     <div>
-      <Header />
       <div style={{ textAlign: 'center' }}>
         <h1>Sources</h1>
         <p>Here you can list the sources of your data.</p>
