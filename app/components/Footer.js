3f80a7f966a556e59a68da9ee68717017a28d3d2
Brenton Lian <brenton.lian@gmail.com>
2024-07-22 13:25:15 -0400

    footer added

---
+++ app/components/Footer.js
@@ -0,0 +1,19 @@
+// components/Footer.js
+const Footer = () => {
+    return (
+      <footer style={footerStyle}>
+        <p>https://github.com/brentonlian</p>
+      </footer>
+    );
+  };
+  
+  const footerStyle = {
+    backgroundColor: '#f8f9fa',
+    padding: '1rem',
+    textAlign: 'center',
+    borderTop: '1px solid #e7e7e7',
+    marginTop: '2rem',
+  };
+  
+  export default Footer;
+  
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 // app/layout.js
 import '../styles/globals.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export const metadata = {
   title: 'Storage Cost Calculator',
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
